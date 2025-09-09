743a38bfed7e461085c3ce69da533903bbd5d9b5
ana.pulsati <ana.pulsati@oncologiador.corp>
2025-09-09 11:05:38 -0300

    Adicionando testes GraphQL

--- graphql/server.js
+++ graphql/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`GraphQL server running on port ${PORT}`);
+  console.log(`GraphQL server running on port ${PORT}/graphql`);
 });
---
+++ test/graphql/external/checkout-graphql.test.js
@@ -0,0 +1,59 @@
+// Bibliotecas
+const request = require('supertest');
+const { expect } = require('chai');
+require('dotenv').config();
+
+// Testes
+describe('Checkout', () => {
+    describe('POST /login', () => {
+        beforeEach(async () => {
+            const respostaLogin = await request(process.env.BASE_URL_GRAPHQL)
+                .post('/api/users/login')
+                .send({
+                    query: `
+                        mutation {
+                            login(email: "ana@gmail.com", password: "123456") {
+                                token
+                            }
+                        }
+                    `
+                });
+            token = respostaLogin.body.data.login.token;
+    
+        });
+
+        it('Deve retornar erro ao tentar registrar email já cadastrado', async () => {
+            const resposta = await request(process.env.BASE_URL_GRAPHQL)
+                .post('/api/register')
+                .send({
+                    query: `
+                        mutation {
+                            register(name: "analigia", email: "ana@gmail.com", password: "123456") {
+                                name
+                                email}
+                        } 
+                    `
+                });
+                    
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.errors[0].message).to.equal("Email já cadastrado");
+        });
+
+        it('Deve calcular o valor final quando informo produto, quantidade e frete', async () => {
+            const resposta = await request(process.env.BASE_URL_GRAPHQL)
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    query: `
+                        mutation {
+                            checkout(items: [{ productId: 1, quantity: 2 }],freight: 10, paymentMethod: "boleto") {
+                                valorFinal}
+                        }   
+                    `
+                });
+                    
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.data.checkout.valorFinal).to.equal(210)
+        });
+    });
+});
